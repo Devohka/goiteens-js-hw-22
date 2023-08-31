4d67a68fc69e71dd49643d5b03f4cb9d0fe7a580
Devohka <110058157+Devohka@users.noreply.github.com>
2023-08-31 20:27:54 +0300

    Hw-22

--- js/task-3_storage.js
+++ js/task-3_storage.js
@@ -1,12 +1,43 @@
-const storage = new Storage([
-    'Нанітоіди',
-    'Пролонгер',
-    'Залізні жупи',
-    'Антигравітатор',
-    ]);
-    const items = storage.getItems();
-    console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
-    storage.addItem('Дроїд');
-    console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
-    storage.removeItem('Пролонгер');
-    console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+// const storage = new Storage([
+//     'Нанітоіди',
+//     'Пролонгер',
+//     'Залізні жупи',
+//     'Антигравітатор',
+//     ]);
+//     const items = storage.getItems();
+//     console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+//     storage.addItem('Дроїд');
+//     console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+//     storage.removeItem('Пролонгер');
+//     console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+// const Storage = function(item){
+//     this.items = items;
+//     {    getItems() {
+//             return this.items;
+//         }
+    
+//         addItem(item) {
+//             this.items.push(item);
+//             return this.items;
+//         }
+    
+//         removeItem(item) {
+//             const index = this.items.indexOf(item);
+//             if (index !== -1) {
+//                 this.items.splice(index, 1);
+//             }
+//         }};
+//     }
+    
+//     const items = [
+//         'Нанітоіди',
+//         'Залізні жупи',
+//         'Дроїд',
+//     ];
+    
+//     const newStorage = new Storage(items);
+//     newStorage.addItem("Lego castle");
+//     newStorage.removeItem("Пролонгер");
+//     newStorage.getItems();
+//     console.log(newStorage);
